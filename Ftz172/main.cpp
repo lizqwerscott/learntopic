e4c1cbfc303d32ab4d2ce08c5cc93cabd870cc3d
lizqwerscott <m13994059075@163.com>
2019-09-22 14:15:28 +0800

    maybe finish Ftz172, need fix

--- Ftz172/main.cpp
+++ Ftz172/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <vector>
 
+int countLastZeros(int number);
+  
 int main()
 {
-
+  std::cout << "fhfj"<< std::endl;
+  int n = 100;
+  int lastZeros = 0;
+  int lastNumber = 5;
+  for (int i = 2; i <= n; i++)
+  {
+    for (int j = lastNumber; j <= n; j = j + 5)
+    {
+      int nzeros = countLastZeros(i * j);
+std::cout << ":" << nzeros << std::endl;
+      if (nzeros > 0)
+      {
+        lastNumber = j + 5;
+        lastZeros = lastZeros + nzeros;
+      }
+    }
+  }
+  std::cout << ":" << lastZeros << std::endl;
   return 0;
 }
+
+int countLastZeros(int number)
+{
+  int lzeros = 0;
+  for (double i = number / 10; i == (int)i; i = i / 10)
+  {
+    lzeros++;
+  }
+  return lzeros;
+}
+
